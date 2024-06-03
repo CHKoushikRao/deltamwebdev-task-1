1619f098ef83a71bce304822c55c1db30d092507
CHKoushikRao <157813820+CHKoushikRao@users.noreply.github.com>
2024-06-04 01:01:43 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,297 @@
+function coloring() {
+    const color =  document.querySelectorAll('.box')
+
+    color.forEach(color =>{
+        getId=color.id
+        arr=Array.from(getId)
+        arr.shift()
+        aside=eval(arr.pop())
+        aup =eval(arr.shift())
+        a=aside+aup
+
+        if(a % 2 == 0){
+            color.style.backgroundColor= 'rgb(67, 184, 61)'
+        }
+        if (a % 2 !== 0){
+            color.style.backgroundColor='rgb(245,222,179)'
+        }
+    })   
+}
+coloring()
+
+alert("hello");
+
+function insertImage() {
+
+    document.querySelectorAll('.box').forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            
+
+                image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.jpg" alt="">`
+                image.style.cursor = 'pointer'
+        }
+        if(image.innerHTML=="Rcanon"){
+            
+
+        }
+    })
+}
+insertImage()
+
+
+
+
+
+
+
+tog = 1
+redChance=true
+blueChance=true
+ 
+document.querySelectorAll('.box').forEach(item => {
+
+
+
+    item.addEventListener('click', function () {
+
+    
+
+
+
+
+        getId = item.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        arr.push('0')
+        aup = eval(arr.join(''))
+        a = aside + aup
+
+
+
+        // Function to display the available paths for all pieces
+
+        function whosTurn(toggle) {
+
+           
+
+            // KING
+
+            if (item.innerText == `${toggle}titan`) {
+
+                if (aside < 8) {
+                    document.getElementById(`b${a + 1}`).style.backgroundColor = 'green'
+
+                }
+                if (aside > 1) {
+
+                    document.getElementById(`b${a - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800) {
+
+                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100) {
+
+                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+                }
+
+                if (aup > 100 && aside < 8) {
+
+                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100 && aside > 1) {
+
+                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside < 8) {
+
+                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside > 1) {
+
+                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
+                }
+                
+                if(blueChance==true && a==105 && document.getElementById('b106').innerText== '' && document.getElementById('b107').innerText== '' && document.getElementById('b108').innerText== 'Wrook'){
+                    document.getElementById(`b107`).style.backgroundColor = 'aqua'
+
+                }
+                if(blueChance==true && a==105 && document.getElementById('b104').innerText== '' && document.getElementById('b103').innerText== '' && document.getElementById('b102').innerText== '' && document.getElementById('b101').innerText== 'Wrook'){
+                    document.getElementById(`b103`).style.backgroundColor = 'aqua'
+
+                }
+                if(redChance==true && a==805 && document.getElementById('b806').innerText== '' && document.getElementById('b807').innerText== '' && document.getElementById('b808').innerText== 'Brook'){
+                    document.getElementById(`b807`).style.backgroundColor = 'aqua'
+
+                }
+                if(redChance==true && a==805 && document.getElementById('b804').innerText== '' && document.getElementById('b803').innerText== '' && document.getElementById('b802').innerText== '' && document.getElementById('b801').innerText== 'Brook'){
+                    document.getElementById(`b803`).style.backgroundColor = 'aqua'
+
+                }
+
+                item.style.backgroundColor = 'pink'
+
+            }
+
+
+            
+            
+        }
+
+
+        // Toggling the turn
+
+        if (tog % 2 !== 0) {
+            document.getElementById('tog').innerText = "Blue's Turn"
+            whosTurn('B')
+        }
+        if (tog % 2 == 0) {
+            document.getElementById('tog').innerText = "Red's Turn"
+            whosTurn('R')
+        }
+
+        reddish()
+
+
+
+        // winning()
+
+       
+
+
+    })
+
+})
+
+
+
+
+
+// Moving the element
+document.querySelectorAll('.box').forEach(item => {
+
+    item.addEventListener('click', function () {
+
+
+        if (item.style.backgroundColor == 'pink') {
+
+            pinkId = item.id
+            pinkText = item.innerText
+
+            document.querySelectorAll('.box').forEach(item2 => {
+
+                item2.addEventListener('click', function () {
+
+                    getId = item2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    arr.push('0')
+                    aup = eval(arr.join(''))
+                    a = aside + aup
+
+                    if (item2.style.backgroundColor == 'green' && item2.innerText.length == 0) {
+
+                        if (pinkText == `Wpawn` && aup == 800) {
+
+                            document.getElementById(`b${a}`).innerText = 'Wqueen'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else if (pinkText == `Bpawn` && aup == 100) {
+
+                            document.getElementById(`b${a}`).innerText = 'Bqueen'
+                            document.getElementById(pinkId).innerText = ''
+                            coloring()
+                            insertImage()
+
+                        }
+                        else {
+
+
+
+                            document.getElementById(pinkId).innerText = ''
+                            item2.innerText = pinkText
+                            coloring()
+                            insertImage()
+                        }
+
+                    }
+
+                    else if (item2.style.backgroundColor == 'aqua') {
+                        if(item2.id=='b103'){
+                            document.getElementById('b101').innerText = ''
+                            document.getElementById('b102').innerText = ''
+                            document.getElementById('b103').innerText = 'Wking'
+                            document.getElementById('b104').innerText = 'Wrook'
+                            document.getElementById('b105').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            blueChance=false
+                            coloring()
+                            insertImage()
+                            
+                        }
+                        else if(item2.id=='b107'){
+                            document.getElementById('b105').innerText = ''
+                            document.getElementById('b106').innerText = 'Wrook'
+                            document.getElementById('b107').innerText = 'Wking'
+                            document.getElementById('b108').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            blueChance=false
+                            coloring()
+                            insertImage()
+
+                        }
+                        else if(item2.id=='b803'){
+                            document.getElementById('b801').innerText = ''
+                            document.getElementById('b802').innerText = ''
+                            document.getElementById('b803').innerText = 'Bking'
+                            document.getElementById('b804').innerText = 'Brook'
+                            document.getElementById('b805').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            redChance=false
+                            coloring()
+                            insertImage()
+                            
+                        }
+                        else if(item2.id=='b807'){
+                            document.getElementById('b805').innerText = ''
+                            document.getElementById('b806').innerText = 'Brook'
+                            document.getElementById('b807').innerText = 'Bking'
+                            document.getElementById('b808').innerText = ''
+                            document.getElementById(pinkId).innerText = ''
+                            redChance=false
+                            coloring()
+                            insertImage()
+
+                        }
+                    }
+
+                })
+            })
+
+        }
+
+    })
+
+})
+
+
+
+
+
+
+// Prvents from selecting multiple elements
+z = 0
+document.querySelectorAll('.box').forEach(ee => {
+    ee.addEventListener('click', function () {
+        z = z + 1
+        if (z % 2 == 0 && ee.style.backgroundColor !== 'green' && ee.style.backgroundColor !== 'aqua') {
+            coloring()
+        }
+    })
+})
